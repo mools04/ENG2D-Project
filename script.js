9787684ef8cbb0d4b438e4c765104fa847e5253f
mools04 <56056790+mools04@users.noreply.github.com>
2019-12-13 09:01:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function button(element){
+function yes(){
 	document.getElementById(element).className = 'dissapear';
 	document.getElementById(element).className = 'appear';
 }
 function no(){
 	alert('Are you sure?');
+}
+function marryThem(){
+}
+function refuse(){
 }
