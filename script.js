7731b99f781df3f910df8c9a5851811ca0226b36
mools04 <56056790+mools04@users.noreply.github.com>
2019-12-12 22:42:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function yes(){
+	document.getElementById('intro').className = 'dissapear';
+	document.getElementById('section1').className = 'appear';
+}
+function no(){
+	alert('Are you sure?');
+}
+function marryThem(){
+
+}
+function refuse(){
+	
+}
