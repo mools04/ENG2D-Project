831ebf9760f3c84922532bdc9288884f45913154
mools04 <56056790+mools04@users.noreply.github.com>
2019-12-13 13:52:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function no(){
 }
 function marryThem(){
 	document.getElementById('section1').className = 'dissapear';
-	document.getElementById('marryBranch').className = 'appear';
+	document.getElementById('marryBranch').className = 'appear2';
 }
 function refuse(){
+	document.getElementById('section1').className = 'dissapear';
+  	document.getElementById('refuseBranch').className = 'appear2';
 }
